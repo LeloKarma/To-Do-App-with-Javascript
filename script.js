e02fb77bb17acb7ca445a6c893d2d1a89ff6a1c7
LeloKarma <92229998+LeloKarma@users.noreply.github.com>
2023-12-07 12:28:14 +0100

    Add files via upload
    This is a To-Do App with Html, Css and JavaScript

---
+++ script.js
@@ -0,0 +1,47 @@
+const listContainer = document.getElementById("list-container");
+const inputBox = document.getElementById("input-box");
+
+ function addTask(){
+     if(inputBox.value === ''){
+
+        alert("Enter a task");
+     }else{
+        
+        //create the listings
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li)
+
+        //create the remove button
+        let span = document.createElement("span");
+        span.innerHTML =  "\u00d7"; //cross symbol
+        li.appendChild(span);
+     }
+
+     //to clear the inputbox after the user has entered a task
+    inputBox.value = "";
+    saveTask();
+}
+
+
+listContainer.addEventListener("click", function(e){
+
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveTask();
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+});
+
+function saveTask(){
+    //store input to the localStorage
+     loacalStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showTask(){
+
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
